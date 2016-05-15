1dc61def0576555572b0f7ac71a02a72d9d436f3
zhongkui <zhongkui@dev.gz.cmcc>
2016-05-15 21:03:19 +0800

    modify app.js

--- test/app.js
+++ test/app.js
@@ -6,7 +6,7 @@ var request = require('request'),
     vars = require('./../resource/vars');
 
 var streams = [
-     /*{
+     {
         streamName:"571d8a57859a25800c690169_1",
         uri: "rtsp://live:gzydYytLive123@218.201.251.99:5554/h264/ch33/main/av_stream"
      },{
@@ -30,23 +30,23 @@ var streams = [
     },{
         streamName:"572b1d3a984765081ff48218_4",
         uri:"rtsp://admin:admin@218.201.251.99:6554/cam/realmonitor?channel=4&subtype=0"
-    },*/{
-        streamName:" 572819b89789e1301e9bf6ab_1",
+    },{
+        streamName:"572819b89789e1301e9bf6ab_1",
         uri:"rtsp://admin:admin123@218.201.243.247:554/cam/realmonitor?channel=1&subtype=0"
     },{
-        streamName:" 572819b89789e1301e9bf6ab_2",
+        streamName:"572819b89789e1301e9bf6ab_2",
         uri:"rtsp://admin:admin123@218.201.243.247:554/cam/realmonitor?channel=2&subtype=0"
     },{
-        streamName:" 572819b89789e1301e9bf6ab_3",
+        streamName:"572819b89789e1301e9bf6ab_3",
         uri:"rtsp://admin:admin123@218.201.243.247:554/cam/realmonitor?channel=3&subtype=0"
     },{
-        streamName:" 572819b89789e1301e9bf6ab_4",
+        streamName:"572819b89789e1301e9bf6ab_4",
         uri:"rtsp://admin:admin123@218.201.243.247:554/cam/realmonitor?channel=4&subtype=0"
     }
 ]
 
 if (process.argv[2] == 'publishStream' || process.argv[2] == 'releaseStream') {
-    request.post('http://localhost:9090/nvr/'.concat(process.argv[2]), http_header, function (err, res, body) {
+    request.post('http://localhost:9090/nvr'.concat(process.argv[2]), http_header, function (err, res, body) {
         if (err)
             console.error(err)
         else
